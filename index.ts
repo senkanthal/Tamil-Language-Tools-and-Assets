959199217bdfe8da080e9d90e3ad9a499faaf3ca
sivothajan <sivothajan2002@gmail.com>
2025-11-05 15:23:33 +0530

    Refactor: import availableFonts from ./assets/availableFonts and export it as a named export from root instead of re-exporting the module

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import allRegExData from '@/assets/allRegExData/index';
+import availableFonts from './assets/availableFonts';
 
-export * from '@/assets/availableFonts/index';
+export { availableFonts };
 export * from '@/assets/fontRegExData/index';
 export default allRegExData;
