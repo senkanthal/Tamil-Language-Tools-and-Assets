0fc657836e0c5c4943a8aac54bd7a550302cacf9
sivothajan <sivothajan2002@gmail.com>
2025-11-05 15:40:44 +0530

    Refactor: replace '@/assets' alias imports with relative './assets' paths in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import allRegExData from '@/assets/allRegExData/index';
+import allRegExData from './assets/allRegExData/index';
 import availableFonts from './assets/availableFonts';
 
 export { availableFonts };
-export * from '@/assets/fontRegExData/index';
+export * from './assets/fontRegExData/index';
 export default allRegExData;
